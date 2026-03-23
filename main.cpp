fdf6986cb8140818499d238044ab8b2b57c475b0
asnasama <asnabbb@gmail.com>
2026-03-23 09:24:05 +0000

    Change input from cin to argv

--- main.cpp
+++ main.cpp
@@ -2,66 +2,54 @@
 using namespace std;
 
 #include "student.h"
-#define N 3
 int main(int argc, char* argv[]) {
-  // student s1,s2;
+  if (argc < 3 || argc % 2 == 0) {
+    cout << "Usage: " << argv[0] << " [name1 age1] [name2 age2] ..." << endl;
+    return 1;
+  }
 
-  // s1.set_name("Amy");
-  // s1.set_age(25);
-  // s2.set_name("James");
-  // s2.set_age(22);
-  // s1.get_age()>s2.get_age() ? s1.print_name() :s2.print_name();  
-  // cout << "is older";
-
-
-
+  int N=(argc-1)/2;
   student a[N];
   // Set age and set name for the first two ppl
-  int i,age;
-  string n;
+  int i;
    // 1. Ask for N names and N age using cin
+  int arg_index=1;
   for(i=0;i<N;i++)
   {
-    cout << "Student " << i+1 << endl;
-    cout << "Name : ";
-    cin >> n;
-    cout << "Age : ";
-    cin >> age;
-    a[i].set_name(n);
-    a[i].set_age(age);
+    a[i].set_name(argv[arg_index]);
+    a[i].set_age(stoi(argv[arg_index+1]));
+    arg_index += 2;
   }
   //2. Print name and age of all N ppl
-  for(i=0;i<N;i++)
-  {
-    cout << "Student " << i+1 << endl;
-    a[i].print_name();
-    cout << "(Age:" << a[i].get_age() << ")" <<endl;;
-  }
+  // for(i=0;i<N;i++)
+  // {
+  //   cout << "Student " << i+1 << endl;
+  //   a[i].print_name();
+  //   cout << "(Age:" << a[i].get_age() << ")" <<endl;;
+  // }
   //3. Print name and age of 
   //the youngest person
   //Find what the age of the youngest person
   int min=a[0].get_age(),index;
-  for(i=1;i<N;i++)
+  for(i=0;i<N;i++)
   {
     if(min>=a[i].get_age())
     {
       min=a[i].get_age();
-      index=i;
     }
   }
   // Print all info for the yougest person
-  cout << "the yougest age is " << min << " years old";
-  for(i=1;i<N;i++)
+  cout << "Youngest student" << endl;
+  for(i=0;i<N;i++)
   {
     if(min==a[i].get_age())
     {
-      a[index].print_name();
-      cout << " ";
+      a[i].print_name();
+      cout << "Age: " << a[i].get_age() <<endl;;
     }
-    cout <<endl;
   }
   //4. Change input from cin to argv
-  
+  //finished
   
  return 0;
 
